bb69395a0403b1a2d986289064e383f968940910
Efsoone <labayli1907@gmail.com>
2025-11-24 21:55:02 +0300

    2th

--- js/mod.js
+++ js/mod.js
@@ -13,7 +13,7 @@ let modInfo = {
 // Set your version in num and name
 let VERSION = {
 	num: "0.0",
-	name: "Literally nothing",
+	name: "Hi,starting the game.",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
